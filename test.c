bd4652f6879f407521e4618f109fb674cd093332
heabeounMKTO <heabeoun69zin@gmail.com>
2025-07-31 17:58:38 +0700

    initial

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "common_img_utils.h"
+#include <stdlib.h>
+
+int main() {
+  StbImage *img;
+  StbImage *rot_img;
+  HbImgUtilsStatus status;
+  load_image("./debug/testf3.jpg", img, status);
+  rotate_image_by_deg(img, rot_img, 128.0f, status); 
+  if(status == HB_IMG_UTILS_OK) {
+    printf("rotate sucessful!\n");
+  } else {
+    hb_img_util_get_error_message(status);
+  }
+}
