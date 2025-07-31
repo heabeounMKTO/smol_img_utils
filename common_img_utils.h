dcec7fdd17a26929e703b86bc57f42ec028ce5dc
heabeounMKTO <heabeoun69zin@gmail.com>
2025-07-31 18:37:37 +0700

    going HOME

--- common_img_utils.h
+++ common_img_utils.h
@@ -10,111 +10,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// maths constants 
+// maths constants
 #ifndef M_PI_F
-#  define M_PI_F (3.1415926535897932f) /* pi */
+#define M_PI_F (3.1415926535897932f) /* pi */
 #endif
 #ifndef M_PI_2_F
-#  define M_PI_2_F (1.5707963267948966f) /* pi/2 */
+#define M_PI_2_F (1.5707963267948966f) /* pi/2 */
 #endif
 #ifndef M_PI_3_F
-#  define M_PI_3_F (1.0471975511965976f) /* pi/3 */
+#define M_PI_3_F (1.0471975511965976f) /* pi/3 */
 #endif
 #ifndef M_2PI_3_F
-#  define M_2PI_3_F (2.0943951023931953f) /* 2pi/3 */
+#define M_2PI_3_F (2.0943951023931953f) /* 2pi/3 */
 #endif
 
 #ifndef M_PI_6_F
-#  define M_PI_6_F (0.5235987755982988f) /* pi/6 */
+#define M_PI_6_F (0.5235987755982988f) /* pi/6 */
 #endif
 
 #ifndef M_7PI_6_F
-#  define M_7PI_6_F (3.665191429188092f) /* 7pi/6 */
+#define M_7PI_6_F (3.665191429188092f) /* 7pi/6 */
 #endif
 
 #ifndef M_4PI_3_F
-#  define M_4PI_3_F (4.1887902047863905f) /* 4pi/3 */
+#define M_4PI_3_F (4.1887902047863905f) /* 4pi/3 */
 #endif
 
 #ifndef M_3PI_2_F
-#  define M_3PI_2_F (4.71238898038469f) /* 3pi/2 */
+#define M_3PI_2_F (4.71238898038469f) /* 3pi/2 */
 #endif
 
-
 #ifndef M_5PI_6_F
-#  define M_5PI_6_F (2.6179938779914944f) /* 5pi/6 */
+#define M_5PI_6_F (2.6179938779914944f) /* 5pi/6 */
 #endif
 
 #ifndef M_11PI_6_F
-#  define M_11PI_6_F (5.759586531581287f) /* 11pi/6 */
+#define M_11PI_6_F (5.759586531581287f) /* 11pi/6 */
 #endif
-
 
 #ifndef M_PI_4_F
-#  define M_PI_4_F (0.7853981633974830f) /* pi/4 */
+#define M_PI_4_F (0.7853981633974830f) /* pi/4 */
 #endif
 #ifndef M_1_PI_F
-#  define M_1_PI_F (0.3183098861837067f) /* 1/pi */
+#define M_1_PI_F (0.3183098861837067f) /* 1/pi */
 #endif
 #ifndef M_2_PI_F
-#  define M_2_PI_F (0.6366197723675813f) /* 2/pi */
+#define M_2_PI_F (0.6366197723675813f) /* 2/pi */
 #endif
 #ifndef M_1_2PI_F
-#  define M_1_2PI_F (0.1591549430918953f) /* 1/(2*pi) */
+#define M_1_2PI_F (0.1591549430918953f) /* 1/(2*pi) */
 #endif
 #ifndef M_1_4PI_F
-#  define M_1_4PI_F (0.0795774715459476f) /* 1/(4*pi) */
+#define M_1_4PI_F (0.0795774715459476f) /* 1/(4*pi) */
 #endif
 #ifndef M_SQRT_PI_8_F
-#  define M_SQRT_PI_8_F (0.6266570686577501f) /* sqrt(pi/8) */
+#define M_SQRT_PI_8_F (0.6266570686577501f) /* sqrt(pi/8) */
 #endif
 #ifndef M_LN_2PI_F
-#  define M_LN_2PI_F (1.8378770664093454f) /* ln(2*pi) */
+#define M_LN_2PI_F (1.8378770664093454f) /* ln(2*pi) */
 #endif
 
 /* Multiplication */
 #ifndef M_2PI_F
-#  define M_2PI_F (6.2831853071795864f) /* 2*pi */
+#define M_2PI_F (6.2831853071795864f) /* 2*pi */
 #endif
 #ifndef M_4PI_F
-#  define M_4PI_F (12.566370614359172f) /* 4*pi */
+#define M_4PI_F (12.566370614359172f) /* 4*pi */
 #endif
 #ifndef M_PI_4F
-#  define M_PI_4F 0.78539816339744830962f /* pi/4 */
+#define M_PI_4F 0.78539816339744830962f /* pi/4 */
 #endif
 
 /* Float sqrt variations */
 #ifndef M_SQRT2_F
-#  define M_SQRT2_F (1.4142135623730950f) /* sqrt(2) */
+#define M_SQRT2_F (1.4142135623730950f) /* sqrt(2) */
 #endif
 #ifndef M_CBRT2_F
-#  define M_CBRT2_F 1.2599210498948732f /* cbrt(2) */
+#define M_CBRT2_F 1.2599210498948732f /* cbrt(2) */
 #endif
 #ifndef M_SQRT1_2F
-#  define M_SQRT1_2F 0.70710678118654752440f /* sqrt(1/2) */
+#define M_SQRT1_2F 0.70710678118654752440f /* sqrt(1/2) */
 #endif
 #ifndef M_SQRT3_F
-#  define M_SQRT3_F (1.7320508075688772f) /* sqrt(3) */
+#define M_SQRT3_F (1.7320508075688772f) /* sqrt(3) */
 #endif
 #ifndef M_LN2_F
-#  define M_LN2_F (0.6931471805599453f) /* ln(2) */
+#define M_LN2_F (0.6931471805599453f) /* ln(2) */
 #endif
 #ifndef M_LN10_F
-#  define M_LN10_F (2.3025850929940457f) /* ln(10) */
+#define M_LN10_F (2.3025850929940457f) /* ln(10) */
 #endif
-
-
 
 #define DEG2RAD(x) ((x) * M_PI_F / 180.0)
 #define RAD2DEG(x) ((x) * 180.0 / M_PI_F)
 
-
 typedef struct {
-  unsigned char* image;
+  unsigned char *image;
   int width, height, channels;
 } StbImage;
-
-
 
 typedef enum {
   HB_IMG_UTILS_OK,
@@ -122,24 +115,26 @@ typedef enum {
   HB_IMG_UTILS_FAILURE_TO_ALLOCATE_IMAGE_MEMORY
 } HbImgUtilsStatus;
 
-static inline void hb_img_util_get_error_message(const HbImgUtilsStatus* status) {
+static inline void
+hb_img_util_get_error_message(const HbImgUtilsStatus *status) {
 
-  switch(*status) {
-    case HB_IMG_UTILS_OK:
+  switch (*status) {
+  case HB_IMG_UTILS_OK:
     fprintf(stdout, "everything ok");
     break;
-    case HB_IMG_UTILS_FAILURE_TO_LOAD_IMAGE:
+  case HB_IMG_UTILS_FAILURE_TO_LOAD_IMAGE:
     fprintf(stderr, "image loading failed!");
     break;
-    case HB_IMG_UTILS_FAILURE_TO_ALLOCATE_IMAGE_MEMORY:
+  case HB_IMG_UTILS_FAILURE_TO_ALLOCATE_IMAGE_MEMORY:
     fprintf(stderr, "memory allocation failed");
     break;
   }
 }
 
-static inline void load_image(const char* filename, StbImage* img, HbImgUtilsStatus* status) {
+static inline void load_image(const char *filename, StbImage *img,
+                              HbImgUtilsStatus *status) {
   int width, height, channels;
-  unsigned char* _img = stbi_load(filename, &width, &height, &channels, 0);  
+  unsigned char *_img = stbi_load(filename, &width, &height, &channels, 0);
   if (!_img) {
     fprintf(stderr, "[ERROR] falied to load image!");
     *status = HB_IMG_UTILS_FAILURE_TO_LOAD_IMAGE;
@@ -147,69 +142,88 @@ static inline void load_image(const char* filename, StbImage* img, HbImgUtilsSta
   }
   img->image = _img;
   img->width = width;
-  img->channels =  channels;
+  img->channels = channels;
   img->height = height;
   *status = HB_IMG_UTILS_OK;
 }
 
-
-static inline void rotate_image_by_eye(const StbImage* src_image, StbImage* dst_image, const float left_eye[2], const float right_eye[2], HbImgUtilsStatus* status) {
-
+static inline void rotate_image_by_eye(const StbImage *src_image,
+                                       StbImage *dst_image,
+                                       const float left_eye[2],
+                                       const float right_eye[2],
+                                       HbImgUtilsStatus *status) {
+  float x1 = left_eye[0];
+  float y1 = left_eye[1];
+  float x2 = right_eye[0];
+  float y2 = right_eye[1];
+  
+  float dx = x2 - x1;
+  float dy = y2 - y1;
+  float angle_rad = atan2(dy, dx);
+  fprintf(stdout, "angle_rad %d", angle_rad);
 }
 
-static inline void rotate_image_by_deg(const StbImage* src_image, StbImage* dst_image, const float rotation_degree, HbImgUtilsStatus* status) {
+
+
+
+static inline void rotate_image_by_deg(const StbImage *src_image,
+                                       StbImage *dst_image,
+                                       const float rotation_degree,
+                                       HbImgUtilsStatus *status) {
   float angle_rad;
-  switch((int) rotation_degree) {
-    case 360:
+  switch ((int)rotation_degree) {
+  case 360:
     angle_rad = M_PI_2_F;
     break;
-    case 0:
+  case 0:
     angle_rad = M_PI_2_F;
     break;
-    case 30:
+  case 30:
     angle_rad = M_PI_6_F;
     break;
-    case 60:
+  case 60:
     angle_rad = M_PI_3_F;
     break;
-    case 90:
+  case 90:
     angle_rad = M_PI_2_F;
     break;
-    case 120:
+  case 120:
     angle_rad = M_2PI_3_F;
     break;
-    case 150:
+  case 150:
     angle_rad = M_5PI_6_F;
-    break; 
-    case 180:
+    break;
+  case 180:
     angle_rad = M_PI_F;
     break;
-    case 210:
+  case 210:
     angle_rad = M_7PI_6_F;
     break;
-    case 240:
+  case 240:
     angle_rad = M_4PI_3_F;
     break;
-    case 270:
+  case 270:
     angle_rad = M_3PI_2_F;
     break;
-    case 300:
+  case 300:
     angle_rad = M_5PI_6_F;
     break;
-    case 330:
+  case 330:
     angle_rad = M_11PI_6_F;
     break;
-    default:
+  default:
     angle_rad = DEG2RAD(rotation_degree);
     break;
   }
 
-
   float cos_theta = cosf(angle_rad);
   float sin_theta = sinf(angle_rad);
-  float new_width = (int)(fabs(src_image->width * cos_theta) + fabs(src_image->height * sin_theta));
-  float new_height = (int)(fabs(src_image->width * sin_theta) + fabs(src_image->height * cos_theta));
-  // unsigned char* rotated = calloc(new_width * new_height * src_image->channels, 1);
+  float new_width = (int)(fabs(src_image->width * cos_theta) +
+                          fabs(src_image->height * sin_theta));
+  float new_height = (int)(fabs(src_image->width * sin_theta) +
+                           fabs(src_image->height * cos_theta));
+  // unsigned char* rotated = calloc(new_width * new_height *
+  // src_image->channels, 1);
   dst_image->image = calloc(new_width * new_height * src_image->channels, 1);
   if (!dst_image->image) {
     *status = HB_IMG_UTILS_FAILURE_TO_ALLOCATE_IMAGE_MEMORY;
@@ -221,33 +235,36 @@ static inline void rotate_image_by_deg(const StbImage* src_image, StbImage* dst_
   float ncy = new_height / 2.0f;
   for (int y = 0; y < new_height; y++) {
     for (int x = 0; x < new_width; x++) {
-            // Translate to origin
-            float dx = x - ncx;
-            float dy = y - ncy;
+      // Translate to origin
+      float dx = x - ncx;
+      float dy = y - ncy;
 
-            // Rotate backward
-            float src_x = cos_theta * dx + sin_theta * dy + cx;
-            float src_y = -sin_theta * dx + cos_theta * dy + cy;
+      // Rotate backward
+      float src_x = cos_theta * dx + sin_theta * dy + cx;
+      float src_y = -sin_theta * dx + cos_theta * dy + cy;
 
-            // Nearest-neighbor lookup
-            int isrc_x = (int)(src_x + 0.5f);
-            int isrc_y = (int)(src_y + 0.5f);
+      // Nearest-neighbor lookup
+      int isrc_x = (int)(src_x + 0.5f);
+      int isrc_y = (int)(src_y + 0.5f);
 
-            if (isrc_x >= 0 && isrc_x < src_image->width && isrc_y >= 0 && isrc_y < src_image->height) {
-              for (int c = 0; c < src_image->channels; c++) {
-                int dst_index = (y * new_width + x) * src_image->channels + c;
-                int src_index = (isrc_y * src_image->width + isrc_x) * src_image->channels + c;
-                dst_image->image[dst_index] = src_image->image[src_index];}
-            }
+      if (isrc_x >= 0 && isrc_x < src_image->width && isrc_y >= 0 &&
+          isrc_y < src_image->height) {
+        for (int c = 0; c < src_image->channels; c++) {
+          int dst_index = (y * new_width + x) * src_image->channels + c;
+          int src_index =
+              (isrc_y * src_image->width + isrc_x) * src_image->channels + c;
+          dst_image->image[dst_index] = src_image->image[src_index];
         }
+      }
+    }
   }
   dst_image->channels = src_image->channels;
-  dst_image->width = (int) new_width;
-  dst_image->height = (int) new_height;
-  stbi_write_png("./debug/rotated_51deg.png", new_width, new_height, src_image->channels, dst_image->image, new_width * src_image->channels);
+  dst_image->width = (int)new_width;
+  dst_image->height = (int)new_height;
+  stbi_write_png("./debug/rotated_51deg.png", new_width, new_height,
+                 src_image->channels, dst_image->image,
+                 new_width * src_image->channels);
   *status = HB_IMG_UTILS_OK;
 }
 
-
-
-#endif //HB_IMG_UTILS END
+#endif // HB_IMG_UTILS END
--- test.c
+++ test.c
@@ -7,6 +7,7 @@ int main() {
   HbImgUtilsStatus status;
   load_image("./debug/testf3.jpg", &img, &status);
   rotate_image_by_deg(&img, &rot_img, 128.0f, &status); 
+  
   if(status == HB_IMG_UTILS_OK) {
     printf("rotate sucessful!\n");
   } else {
